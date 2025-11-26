1b3164e75b015b29291539a4c44b5080ab0d0aaf
MarianoMaldonado-dev <mariano.maldonado289908@alumnos.info.unlp.edu.ar>
2025-11-26 12:25:25 -0300

    Ajuste de configuraciones para deploy

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,4 @@
-/*import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-    base: '/ShopEasy/',
-    plugins: [react()],
-})*/
-
-/*
-* NOTA: Éste archivo de configuración es necesario para el deploy de GitHub Pages.
-*/
-
-/* Esta siguiente configuración es para que no dé error al intentar lanzar el proyecto en local */import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
